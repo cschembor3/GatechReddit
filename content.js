50694d28f55ce12a546b279f2c48c96bfa543846
cschembor3 <cschembor3@gatech.edu>
2015-09-23 19:50:15 -0400

    opens urls with certain keywords

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
-var key = 'https://t-square.gatech.edu/portal/site/gtc'
-//var key = "mail"
+//var key = 'https://t-square.gatech.edu/portal/site/gtc'
+var key = "georgia tech"
 function openLinks() {
 	var tracker = [];
 	var checker = 0;
 	for (i = 0; i < document.links.length; i++) {
+		//alert(document.links[i].href);
 		if (document.links[i].href.match(key)) {
 			for (j = 0; j < tracker.length; j++) {
 				if (document.links[i].href === (tracker[j])) {
-					alert(checker);
+					//alert(checker);
 					checker++;
 				}
 			}
 			if (checker == 0) {
 				tracker.push(document.links[i].href);
 				var win = window.open(document.links[i].href);
-				win.focus();	
+				win.focus();
+			//alert("WOW");	
 			}
 		}
 	}
