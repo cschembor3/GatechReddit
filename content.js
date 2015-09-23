b4bbefb705f537c99a2e2d9be64a161fec02ca2f
cschembor3 <cschembor3@gatech.edu>
2015-09-22 21:24:42 -0400

    Opens all class pages upon entering t2

--- content.js
+++ content.js
@@ -1,11 +1,24 @@
 var key = 'https://t-square.gatech.edu/portal/site/gtc'
-
+//var key = "mail"
 function openLinks() {
+	var tracker = [];
+	var checker = 0;
 	for (i = 0; i < document.links.length; i++) {
 		if (document.links[i].href.match(key)) {
-			var win = window.open(document.links[i].href);
-			win.focus();
+			for (j = 0; j < tracker.length; j++) {
+				if (document.links[i].href === (tracker[j])) {
+					alert(checker);
+					checker++;
+				}
+			}
+			if (checker == 0) {
+				tracker.push(document.links[i].href);
+				var win = window.open(document.links[i].href);
+				win.focus();	
+			}
 		}
 	}
 }
-//alert("Hi")
+
+//alert(document.links.length)
+window.onload = openLinks();
