5116873e1afe0ee93d07df063b519c482e67e221
cschembor3 <cschembor3@gatech.edu>
2015-09-23 22:34:21 -0400

    successfully opens all threads with certain GT keywords in the thread title

--- content.js
+++ content.js
@@ -1,26 +1,19 @@
-//var key = 'https://t-square.gatech.edu/portal/site/gtc'
-var key = "georgia tech"
-function openLinks() {
-	var tracker = [];
-	var checker = 0;
-	for (i = 0; i < document.links.length; i++) {
-		//alert(document.links[i].href);
-		if (document.links[i].href.match(key)) {
-			for (j = 0; j < tracker.length; j++) {
-				if (document.links[i].href === (tracker[j])) {
-					//alert(checker);
-					checker++;
-				}
-			}
-			if (checker == 0) {
-				tracker.push(document.links[i].href);
-				var win = window.open(document.links[i].href);
-				win.focus();
-			//alert("WOW");	
-			}
+var key = ["georgia tech", "yellow jackets", "justin thomas"];
+
+var openPage = function (thread, url) {
+	var win = window.open(url);
+	win.focus();
+}
+
+var checkTitle = function (itemNum, itemDomEl) {
+	var $thread = $(itemDomEl);
+	var title = $thread.text();
+	var url = $thread.attr("href");
+	for (i = 0; i < key.length; i++) {
+		if (title && title.toLowerCase().indexOf(key[i]) > 0) {
+			openPage(title, url);
 		}
 	}
 }
 
-//alert(document.links.length)
-window.onload = openLinks();
+$("p.title a.title").each(checkTitle);
